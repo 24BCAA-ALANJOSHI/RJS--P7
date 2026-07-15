bc477e52d0905d705e2f9a769908f717f696fff3
24BCAA-ALANJOSHI <alanjoshi408@gmail.com>
2026-07-15 10:12:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const element = React.createElement("h1",null,"Welcome to React");
 
-const root = ReactDOM.createroot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 
 root.render(element);
